95a316803ddd6e9265c0ab85836e6e312c1e6e7a
Michel Magdy <85393466+Michel-Magdy09@users.noreply.github.com>
2022-07-21 15:10:37 +0200

    done

--- lib/categories/electronicsscreen.dart
+++ lib/categories/electronicsscreen.dart
@@ -10,18 +10,35 @@ class ElectronicsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var cubit = BlocCubit.get(context);
     return BlocConsumer<BlocCubit, BlocState>(
-        listener: (context, state)
-    {},
-    builder: (context, state) {
-    return GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 2,
-    crossAxisSpacing: 4.0,
-    mainAxisSpacing: 4.0
-    ),
-    itemBuilder: (context,index){
-      return Text("hello");
-      });
-    });
-    
-    }
+        listener: (context, state) {},
+        builder: (context, state) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  // color: Colors.grey,
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    // scrollDirection: Axis.vertical,
+                    physics: ScrollPhysics(),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                      childAspectRatio: 0.6,
+
+                    ),
+                    itemCount: 6,
+                    itemBuilder: (context,index){
+                      return ProductItem(cubit.electroData[index]);
+                    },
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+              ],
+            ),
+          );
+        });
   }
+}
