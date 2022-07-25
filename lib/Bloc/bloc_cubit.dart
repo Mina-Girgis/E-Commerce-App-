4b55879c0c52674baa53f8e085ddbbc9aa06a3bd
Michel Magdy <85393466+Michel-Magdy09@users.noreply.github.com>
2022-07-25 14:21:25 +0200

    sidebar updated

--- lib/Bloc/bloc_cubit.dart
+++ lib/Bloc/bloc_cubit.dart
@@ -8,161 +8,171 @@ import 'package:e_commerce/Shared/Components/Network/Remote/diohelper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+
 part 'bloc_state.dart';
 
 class BlocCubit extends Cubit<BlocState> {
-    BlocCubit() : super(BlocInitial());
-    static BlocCubit get(context)=> BlocProvider.of(context);
+  BlocCubit() : super(BlocInitial());
+  static BlocCubit get(context) => BlocProvider.of(context);
 
-    int titleIndex = 0 ;
+  int titleIndex = 0;
 
-    dynamic currentUserId = 0;
+  int sideBarIndex = 0;
 
-    int productQuantity = 1;
+  dynamic currentUserId = 0;
 
-    List<int>nums =[1,2,3];
-    Color colorRed = Colors.red;
-    Color colorBlack = Colors.black38;
+  int productQuantity = 1;
 
-    bool searchItem (int id){
-      for(int i =0 ; i <nums.length;i++){
-        if(id == nums[i])return true;
+  List<int> nums = [1, 2, 3];
+  Color colorRed = Colors.red;
+  Color colorBlack = Colors.black38;
+
+  bool searchItem(int id) {
+    for (int i = 0; i < nums.length; i++) {
+      if (id == nums[i]) return true;
+    }
+    return false;
+  }
+
+  List<User> users = UsersDatabase.userData;
+  List<dynamic> _AllData = [];
+  List<dynamic> _Electronics = [];
+  List<dynamic> _MenClothing = [];
+  List<dynamic> _WomenClothing = [];
+  List<dynamic> _Jewelery = [];
+  Map<dynamic, dynamic> _SpecificProduct = {};
+
+  List<Model> allData = [];
+  List<Model> menData = [];
+  List<Model> womenData = [];
+  List<Model> jeweleryData = [];
+  List<Model> electroData = [];
+  List<Model> SpecificProductData = [];
+  Model model = Model();
+
+  List<Model> cartData = [];
+
+  void AddProductInCart(Model model) {
+    cartData.add(model);
+    print("Product added !!");
+    emit(AddProductInCartSuccess());
+  }
+
+  void RemoveProductInCart(int index) {
+    cartData.removeAt(index);
+    print("Product removed !!");
+    emit(RemoveProductInCartSuccess());
+  }
+
+  void ChangeProductQuantityPlus() {
+    productQuantity++;
+    emit(ChangeProductQuantityPlusSuccess());
+  }
+
+  void ChangeProductQuantityMinus() {
+    productQuantity--;
+    emit(ChangeProductQuantityMinusSuccess());
+  }
+
+  void ChangeTitleIndex(int index) {
+    titleIndex = index;
+    emit(CatTitleColor());
+  }
+
+  void ChangeCurrentUserId(int index) {
+    currentUserId = index;
+    emit(ChangeCurrentUserIdSuccess());
+  }
+
+  void getAll() {
+    DioHelper.getData(url: "products", query: {"sort": "asc"}).then((value) {
+      _AllData = value.data;
+      for (int i = 0; i < _AllData.length; i++) {
+        allData.add(Model.fromJson(_AllData[i]));
       }
-      return false;
-    }
+      emit(GetAllDataSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(GetAllDataFail());
+    });
+  }
 
-    List<User> users= UsersDatabase.userData;
-    List <dynamic> _AllData=[];
-    List <dynamic> _Electronics=[];
-    List <dynamic> _MenClothing=[];
-    List <dynamic> _WomenClothing=[];
-    List <dynamic> _Jewelery=[];
-    Map<dynamic,dynamic> _SpecificProduct={};
-
-    List<Model>allData=[];
-    List<Model>menData=[];
-    List<Model>womenData=[];
-    List<Model>jeweleryData=[];
-    List<Model>electroData=[];
-    List<Model>SpecificProductData = [];
-    Model model = Model();
-
-    List<Model>cartData=[];
-
-    void AddProductInCart(Model model){
-      cartData.add(model);
-      print("Product added !!");
-      emit(AddProductInCartSuccess());
-
-    }
-    void RemoveProductInCart(int index){
-      cartData.removeAt(index);
-      print("Product removed !!");
-      emit(RemoveProductInCartSuccess());
-    }
-
-    void ChangeProductQuantityPlus(){
-      productQuantity++;
-      emit(ChangeProductQuantityPlusSuccess());
-    }
-
-    void ChangeProductQuantityMinus(){
-      productQuantity--;
-      emit(ChangeProductQuantityMinusSuccess());
-    }
-
-    void ChangeTitleIndex(int index){
-      titleIndex = index;
-      emit(CatTitleColor());
-    }
-
-    void ChangeCurrentUserId(int index){
-      currentUserId=index;
-      emit(ChangeCurrentUserIdSuccess());
-    }
-
-    void getAll(){
-      DioHelper.getData(url: "products", query: {"sort":"asc"} ).then((value) {
-        _AllData=value.data;
-        for(int i = 0 ; i < _AllData.length ; i++){
-          allData.add(Model.fromJson(_AllData[i]));
-        }
-        emit(GetAllDataSuccess());
-      } ).catchError((error){
-        print(error.toString());
-        emit(GetAllDataFail());
-      });
-    }
-
-    void getElectronics(){
-      DioHelper.getData(url: "products/category/electronics", query: {"sort":"asc"} ).then((value) {
-        _Electronics=value.data;
-        for(int i = 0 ; i < _Electronics.length ; i++){
-          electroData.add(Model.fromJson(_Electronics[i]));
-        }
-        emit(GetElectronicsDataSuccess());
-      } ).catchError((error){
-        print(error.toString());
-        emit(GetElectronicsDataFail());
-      });
-    }
-
-    void getMenClothing(){
-      DioHelper.getData(url: "products/category/men's clothing", query: {"sort":"asc"} ).then((value) {
-        _MenClothing=value.data;
-        for(int i = 0 ; i < _MenClothing.length ; i++){
-          menData.add(Model.fromJson(_MenClothing[i]));
-        }
-        print(menData.length);
-        emit(GetMenClothingDataSuccess());
-      } ).catchError((error){
-        print(error.toString());
-        emit(GetMenClothingDataFail());
-      });
-    }
-
-    void getWomenClothing(){
-      DioHelper.getData(url: "products/category/women's clothing", query: {"sort":"asc"} ).then((value) {
-        _WomenClothing=value.data;
-        for(int i = 0 ; i < _WomenClothing.length ; i++) {
-          womenData.add(Model.fromJson(_WomenClothing[i]));
-        }
-        emit(GetWomenClothingDataSuccess());
-      } ).catchError((error){
-        print(error.toString());
-        emit(GetWomenClothingDataFail());
-      });
-    }
-
-    void getJewelery(){
-      DioHelper.getData(url: "products/category/jewelery", query: {"sort":"asc"} ).then((value) {
-        _Jewelery=value.data;
-        for(int i = 0 ; i < _Jewelery.length ; i++) {
-          jeweleryData.add(Model.fromJson(_Jewelery[i]));
-        }
-        emit(GetJeweleryDataSuccess());
-      } ).catchError((error){
-        print(error.toString());
-        emit(GetJeweleryDataFail());
-      });
-    }
-
-    Model getSpecificItem(int id){
-      // BinarySearch log(n)
-      int start=0;
-      int end = allData.length-1;
-      while(start<=end){
-        int mid = ((start+end)/2).toInt();
-        if(allData[mid].id < id) start=mid+1;
-        else if(allData[mid].id > id)end=mid-1;
-        else {
-          emit(GetSpecificProductDataSuccess());
-          return allData[mid];
-        }
+  void getElectronics() {
+    DioHelper.getData(
+        url: "products/category/electronics",
+        query: {"sort": "asc"}).then((value) {
+      _Electronics = value.data;
+      for (int i = 0; i < _Electronics.length; i++) {
+        electroData.add(Model.fromJson(_Electronics[i]));
       }
-      emit(GetSpecificProductDataFail());
-      return model;
+      emit(GetElectronicsDataSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(GetElectronicsDataFail());
+    });
+  }
+
+  void getMenClothing() {
+    DioHelper.getData(
+        url: "products/category/men's clothing",
+        query: {"sort": "asc"}).then((value) {
+      _MenClothing = value.data;
+      for (int i = 0; i < _MenClothing.length; i++) {
+        menData.add(Model.fromJson(_MenClothing[i]));
+      }
+      print(menData.length);
+      emit(GetMenClothingDataSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(GetMenClothingDataFail());
+    });
+  }
+
+  void getWomenClothing() {
+    DioHelper.getData(
+        url: "products/category/women's clothing",
+        query: {"sort": "asc"}).then((value) {
+      _WomenClothing = value.data;
+      for (int i = 0; i < _WomenClothing.length; i++) {
+        womenData.add(Model.fromJson(_WomenClothing[i]));
+      }
+      emit(GetWomenClothingDataSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(GetWomenClothingDataFail());
+    });
+  }
+
+  void getJewelery() {
+    DioHelper.getData(url: "products/category/jewelery", query: {"sort": "asc"})
+        .then((value) {
+      _Jewelery = value.data;
+      for (int i = 0; i < _Jewelery.length; i++) {
+        jeweleryData.add(Model.fromJson(_Jewelery[i]));
+      }
+      emit(GetJeweleryDataSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(GetJeweleryDataFail());
+    });
+  }
+
+  Model getSpecificItem(int id) {
+    // BinarySearch log(n)
+    int start = 0;
+    int end = allData.length - 1;
+    while (start <= end) {
+      int mid = ((start + end) / 2).toInt();
+      if (allData[mid].id < id)
+        start = mid + 1;
+      else if (allData[mid].id > id)
+        end = mid - 1;
+      else {
+        emit(GetSpecificProductDataSuccess());
+        return allData[mid];
+      }
     }
-
-
+    emit(GetSpecificProductDataFail());
+    return model;
+  }
 }
---
+++ lib/Models/side_bar_data.dart
@@ -0,0 +1,6 @@
+class SideData {
+  dynamic title;
+  dynamic icon;
+
+  SideData({required this.title, required, this.icon});
+}
